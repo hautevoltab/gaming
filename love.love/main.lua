2e10d8544b96b473994ff3daac7a1f98d1e53bf9
hautevoltab <hautevoltab@gmail.com>
2018-10-25 13:34:13 +0200

    youyou

--- love.love/main.lua
+++ love.love/main.lua
@@ -1,10 +1,12 @@
 -- the love.load is for initialised the variable 
 function love.load()
-x = 0
-y = 0
+love.window.setMode(500,500)
+    x = 0
+y = 400
 end
 -- the love.update is for use the input of the keyboard 
 function love.update(dt)
+-- love.keyboard use for change the direction of the pad
     if love.keyboard.isDown('right') then
         x=x+10
     elseif love.keyboard.isDown('left') then
@@ -19,5 +21,5 @@ end
 -- the love.draw is for render the thing in to the windows 
 function love.draw()
     love.graphics.setColor(0,255,255)
-    love.graphics.rectangle('fill',x,y,123,123)
+    love.graphics.rectangle('fill',x,y,12,100)
 end
